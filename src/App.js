b4eb4cbd3b96e6478c338bf6cbfd479eae7d2903
Dev <deobratds@gmail.com>
2022-10-16 13:16:12 +0000

    Update 2 files

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ export default function App() {
     <div className="App">
       <h1>Hello World</h1>
       <h2>Dev is back</h2>
+      <Card>
+           <UserImage src="data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" />
+        </Card>
     </div>
   );
 }
